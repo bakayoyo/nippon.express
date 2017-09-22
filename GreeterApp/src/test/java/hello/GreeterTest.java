b99c01241e41e6e65d5afbb86e43a8390b52ff58
Ronald Veldhuis <veldhuisrg@hotmail.com>
2017-09-22 11:32:40 +0200

    ok

--- GreeterApp/src/test/java/hello/GreeterTest.java
+++ GreeterApp/src/test/java/hello/GreeterTest.java
@@ -1,10 +1,5 @@
 package hello;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 public class GreeterTest {
 
 	private Greeter greeter = new Greeter();
